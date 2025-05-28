2700a005fd3557c974d4981f19546daa29d9c06d
Alexandre K <alexandre.karinthi@gmail.com>
2025-05-28 15:53:08 +0200

    the echo builtin t5

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,24 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-    let mut input = String::new();
+    
     loop{   
         print!("$ ");
         io::stdout().flush().unwrap();
     
     // Wait for user input
-        
+        let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        match input.trim(){
+        let input = input.trim();
+        if input.is_empty(){
+            continue;
+        }
+        let words: Vec<&str> = input.split_whitespace().collect();
+        match words[0]{
             "quit" => break,
             "exit 0" => return,
-            "echo " => println!("{}",input[5..]),
+            "echo" => println!("{}",words[1..].join(" ")),
             string => println!("{}: command not found", string),
         }
-        input = String::new();
     }
 }
