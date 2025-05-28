a3fc1ca7005232c6fbf7dfbb2898fc890193ba9d
Alexandre K <alexandre.karinthi@gmail.com>
2025-05-28 15:20:56 +0200

    the exit builtin t3

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
         
         io::stdin().read_line(&mut input).unwrap();
         match input.trim(){
-            "exit 0" => break,
+            "exit 0" => return,
             string => println!("{}: command not found", string),
         }
     }
