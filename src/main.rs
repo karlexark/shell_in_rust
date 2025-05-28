a6ceee693787da533791f63311d1884080cf23ab
Alexandre K <alexandre.karinthi@gmail.com>
2025-05-28 15:18:16 +0200

    the exit builtin t1

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,18 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
+    let mut input = String::new();
     loop{   
         print!("$ ");
         io::stdout().flush().unwrap();
     
     // Wait for user input
-        let mut input = String::new();
+        
         io::stdin().read_line(&mut input).unwrap();
-        println!("{}: command not found",input.trim());
+        match input.trim(){
+            "quit" => break,
+            "exit 0" => return,
+            string => println!("{}: command not found", string),
+        }
     }
 }
