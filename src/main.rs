3bf575912ae3f46490d91be88b36b2b960ecb204
Alexandre K <alexandre.karinthi@gmail.com>
2025-05-28 15:09:00 +0200

    REPL t2

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    loop{
+    loop{   
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    
     // Wait for user input
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
